94c36906e1d401eda88638c2ac2a46713d476ab3
adcm2 <123085641+adcm2@users.noreply.github.com>
2024-02-08 12:34:58 +0000

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adcm2/raidam/TomographyModels
-# Build directory: /home/adcm2/raidam/TomographyModels/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("tests")
--- build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/adcm2/raidam/TomographyModels/tests
-# Build directory: /home/adcm2/raidam/TomographyModels/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
